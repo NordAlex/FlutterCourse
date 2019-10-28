bc18a47d1df4b6b73d8a1b461c6ede75583f1641
NordAlex <alexhac94@gmail.com>
2019-10-28 20:54:01 +0200

    Update list tile.

--- FlutterBasics/lecture2_todo_app/lib/widgets/todo_list_widget.dart
+++ FlutterBasics/lecture2_todo_app/lib/widgets/todo_list_widget.dart
@@ -8,47 +8,84 @@ class TodoList extends StatefulWidget {
 }
 
 class TodoListState extends State<TodoList> {
-  List<String> items = ['Item 1', 'Item 2', 'Item 3'];
+  final TextEditingController _textFieldController = TextEditingController();
+  final GlobalKey<ScaffoldState> _scaffoldkey =  GlobalKey<ScaffoldState>();
+
+  final _appName = 'TO DO APP';
+  final _itemRemovedText = 'Item is completed. Well done!';
+  final _addItemText ='Write new action todo';
+  final _cancelText = 'Cancel';
+  final _okText = 'Ok';
+
+  List<String> items = [
+    'Complete flutter homework',
+    'Fix all bugs',
+    'Enslave the world',
+    'Date with a girl',
+    'Eat helthy food'
+  ];
 
   Widget _buildList() {
     return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
         itemCount: items.length,
         itemBuilder: (BuildContext ctxt, int index) {
-          return ListTile(title: Card( child: Text(items[index])));
+          return Column(children: <Widget>[
+            ListTile(
+              title: Text(items[index]),
+              trailing: IconButton(
+                icon: const Icon(Icons.done),
+                onPressed: () => _removeItex(index),
+              ),
+            ),
+            Divider()
+          ]);
         });
   }
 
-  TextEditingController _textFieldController = TextEditingController();
-   
-   void _addNewItem(BuildContext context) {
+  void _removeItex(int index) {
+    setState(() {
+      items.removeAt(index);
+    });
+    _showSnackBar();
+  }
+
+void _showSnackBar() {
+    final snackBarContent = SnackBar(
+      content: Text(_itemRemovedText),
+    );
+    _scaffoldkey.currentState.showSnackBar(snackBarContent);
+  }
+
+  void _addNewItem(BuildContext context) {
     showDialog<AlertDialog>(
         context: context,
         builder: (context) {
           return AlertDialog(
-            title: Text('Write new action todo'),
-      content: TextField(
-        controller: _textFieldController,
-        decoration: InputDecoration(hintText: 'Todo action'),
-      ),
+            title: Text(_addItemText),
+            content: TextField(
+              controller: _textFieldController,
+              decoration: InputDecoration(hintText: 'Todo action'),
+            ),
             actions: <Widget>[
               FlatButton(
-                child: Text('CANCEL'),
+                child: Text(_cancelText),
                 onPressed: () {
                   Navigator.of(context).pop();
                 },
               ),
-                 FlatButton(
-            child: Text('Add'),
-            onPressed: () {
-              setState(() {
-                if (_textFieldController.text.length > 0) {
-                  items.add(_textFieldController.text);
-                  _textFieldController.clear();
-                }
-              });
-              Navigator.of(context).pop();
-            },
-          )
+              FlatButton(
+                child: Text(_okText),
+                onPressed: () {
+                  setState(() {
+                    if (_textFieldController.text.isNotEmpty) {
+                      items.add(_textFieldController.text);
+                      _textFieldController.clear();
+                    }
+                  });
+                  Navigator.of(context).pop();
+                },
+              )
             ],
           );
         });
@@ -57,12 +94,13 @@ class TodoListState extends State<TodoList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Todo list")),
+      key: _scaffoldkey,
+        appBar: AppBar(title: Text(_appName)),
         body: _buildList(),
         floatingActionButton: FloatingActionButton(
           onPressed: () => _addNewItem(context),
-          tooltip: 'Add',
-          child: Icon(Icons.add),
+          tooltip: _okText,
+          child: const Icon(Icons.add),
         ));
   }
 }
