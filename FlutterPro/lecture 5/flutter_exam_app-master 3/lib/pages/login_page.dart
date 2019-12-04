6241c226b32b66d6043efcb5ad415405c560373b
NordAlex <alexhac94@gmail.com>
2019-12-04 21:39:00 +0200

    Add 5-th lecture

---
+++ FlutterPro/lecture 5/flutter_exam_app-master 3/lib/pages/login_page.dart
@@ -0,0 +1,82 @@
+import 'package:dating_app/pages/main_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({Key key}) : super(key: key);
+
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Please authorize'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 15, right: 15),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Email',
+                ),
+              ),
+              Container(
+                height: 5,
+              ),
+              TextField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+              ),
+              Container(
+                height: 15,
+              ),
+              RaisedButton(
+                onPressed: () => _login(context),
+                child: const Text('Login'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _login(BuildContext context) async {
+    final email = _emailController.text;
+    final password = _passwordController.text;
+    final _auth = FirebaseAuth.instance;
+    if (email != null && password != null) {
+      try {
+        final authResult = await _auth.signInWithEmailAndPassword(
+            email: email, password: password);
+        _navigateToMain(context, authResult);
+      } catch (ERROR_USER_NOT_FOUND) {
+        final authResult = await _auth.createUserWithEmailAndPassword(
+            email: email, password: password);
+        _navigateToMain(context, authResult);
+      }
+    }
+  }
+
+  void _navigateToMain(BuildContext context, AuthResult result) {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute<void>(
+        builder: (context) => MainPage(
+          firebaseUser: result.user,
+        ),
+      ),
+    );
+  }
+}
